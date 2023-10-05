1ca83564e280b027130e95685a8275fffe1b6679
mimijae <qzqzcaraz00@gmail.com>
2023-10-05 16:01:33 +0900

    admin.py 코드 추가

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# sqlite db 연결
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 # Postgres db 연결
 DATABASES = {
     'default': {
--- marsapp/admin.py
+++ marsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fruit
 
-# Register your models here.
+class FruitAdmin(admin.ModelAdmin):
+    list_display = ['name']  # 관리자 목록 페이지에서 표시할 필드
+    search_fields = ['name']  # 검색 기능을 사용하여 검색할 수 있는 필드
+
+admin.site.register(Fruit, FruitAdmin)
